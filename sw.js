92e46ac296f0ec11816f43397bd3affa26422aa1
jensthaden-eng <jens.thaden@yahoo.com>
2026-09-26 00:06:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE="thaden-material-v16";
+const CACHE="thaden-material-v17";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./sw.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
   if(e.request.mode==="navigate"){
-    e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put("./index.html",copy));return r;}).catch(()=>caches.match("./index.html")));
-  }else{
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+    e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put("./index.html",copy));return r}).catch(()=>caches.match("./index.html")));
+    return;
   }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
